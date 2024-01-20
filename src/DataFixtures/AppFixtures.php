fef34bec4d74c030946ea205130e446b57784c1c
Romuald <romyklk1610@gmail.com>
2024-01-20 10:50:57 +0100

    📦 NEW: ajout procfile

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+
+    }
+}
